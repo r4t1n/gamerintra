f2a12c48ed4f6e1badeb9952fd32c548140227a9
ratin <ratinvr@gmail.com>
2023-06-13 17:11:18 +0200

    Edit main.py

--- gamerintra/main.py
+++ gamerintra/main.py
@@ -6,7 +6,7 @@ url = input("Enter full URL from SkoleIntra: ")
 username = subprocess.check_output("whoami").decode(sys.stdout.encoding).strip()
 calendar_location = "/home/" + username + "/.config/gamerintra/calendar/"
 
-subprocess.run(["mkdir", calendar_location])
+subprocess.run(["mkdir", "-p", calendar_location])
 
-current_time = time.strftime("%H:%M:%S", time.localtime())
+current_time = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
 subprocess.run(["wget", "--quiet", "-O", calendar_location + current_time, url])
