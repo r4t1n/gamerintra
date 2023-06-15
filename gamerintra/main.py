8e0cdde586a0dab0807f68f12862b0a42f219052
ratin <ratinvr@gmail.com>
2023-06-15 11:37:00 +0200

    Simplify variables

--- gamerintra/main.py
+++ gamerintra/main.py
@@ -11,19 +11,21 @@ location = "/home/" + username + "/.config/gamerintra/"
 calendar_location = location + "calendar/"
 log_location = location + "logs/"
 
-feed = "GetFeed.ics"
-feed_new = "GetFeed_new.ics"
+feed = calendar_location + "GetFeed.ics"
+feed_new = calendar_location + "GetFeed_new.ics"
+
+current_time = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+wget_log = log_location + "wget_log_" + current_time
 
 subprocess.run(["mkdir", "-p", calendar_location, log_location])
 subprocess.run(["cp", "../gamerintra.conf", location])
 
-current_time = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-subprocess.run(["wget", "-o", log_location + "wget-log_" + current_time, "-O", calendar_location + feed_new, url])
+subprocess.run(["wget", "-o", wget_log, "-O", feed_new, url])
 
 # "--ignore-matching-lines=^DTSTAMP:................" is needed to ignore the timestamp of downloading since this changes every time you download the file
-# We use "stdout=subprocess.DEVNULL" and "stderr=subprocess.DEVNULL" since I do not want diff printing out "No such file or directory" if you run the script for the first time
+# "stdout=subprocess.DEVNULL" and "stderr=subprocess.DEVNULL" is used to supress diff outputting to the terminal
 diff_process = subprocess.run(
-    ["diff", "-q", "--ignore-matching-lines=^DTSTAMP:................", calendar_location + feed_new, calendar_location + feed],
+    ["diff", "-q", "--ignore-matching-lines=^DTSTAMP:................", feed_new, feed],
     stdout=subprocess.DEVNULL,
     stderr=subprocess.DEVNULL
 )
@@ -32,8 +34,6 @@ diff_process = subprocess.run(
 # Return code 1 = The files are not identical
 # Return code 2 = No such file or directory
 if diff_process.returncode == 0:
-    subprocess.run(["rm", calendar_location + feed_new])
-elif diff_process.returncode == 1:
-    subprocess.run(["mv", calendar_location + feed_new, calendar_location + feed])
-elif diff_process.returncode == 2:
-    subprocess.run(["mv", calendar_location + feed_new, calendar_location + feed])
+    subprocess.run(["rm", feed_new])
+elif diff_process.returncode == 1 or diff_process.returncode == 2:
+    subprocess.run(["mv", feed_new, feed])
